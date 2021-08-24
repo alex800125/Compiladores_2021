1ccc3354c5e0f9f946324e3396b0715d726f7c9b
Alex Venturini <alex800125@hotmail.com>
2021-08-24 09:26:51 -0300

    atualizando lexico

--- Lexico.java
+++ Lexico.java
@@ -1,5 +1,49 @@
 package Compiladores_2021;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileReader;
+
 public class Lexico{
-    // TODO: ainda vai ser editado
+
+    // Variaveis globais
+    Scanner arquivo;
+    String palavra;
+
+    public Lexico(){
+        System.out.println("LEXICO - iniciado");   
+
+        String path = "C:\\Users\\alex8\\Documents\\Compiladores\\Compiladores_2021\\Testes\\teste.txt";
+
+        try {
+            arquivo = new Scanner(new FileReader(path));
+        } catch (IOException e) {
+            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+        }
+    }
+
+    public void analisadorLexical(){
+        System.out.println("LEXICO - analisadorLexical"); 
+
+        while (pegarPalavra()){
+            System.out.println("LEXICO - palavra = " + palavra);
+
+            // implementar a partir daqui
+        }
+
+        arquivo.close();
+    }
+
+    public boolean pegarPalavra(){
+        // System.out.println("LEXICO - pegarPalavra"); 
+
+        boolean resultado = false;
+
+        if (arquivo.hasNext()){
+            resultado = true;
+            palavra = arquivo.next();
+        } 
+
+        return resultado;
+    }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package Compiladores_2021;
 
 public class Main {
     public static void main(String[] arg) {
-        System.out.println("Primeiro commit");        
+        Lexico lexico = new Lexico();     
+        lexico.analisadorLexical();
     }
 }
